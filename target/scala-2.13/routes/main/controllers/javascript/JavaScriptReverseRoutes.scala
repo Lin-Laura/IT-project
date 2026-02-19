afc25a52f031e89194d2a5b1a967c7d5d05d34c0
Lin-Laura <3075229W@student.gla.ac.uk>
2026-02-19 19:22:06 +0000

    Add initial project template

---
+++ target/scala-2.13/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,65 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/wanglin/Desktop/5074 IT+ Team Project/ITSD-DT2025-26-Template/conf/routes
+// @DATE:Mon Feb 16 14:24:48 GMT 2026
+
+import play.api.routing.JavaScriptReverseRoute
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+// @LINE:6
+package controllers.javascript {
+
+  // @LINE:6
+  class ReverseGameScreenController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:6
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.GameScreenController.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "game"})
+        }
+      """
+    )
+  
+    // @LINE:7
+    def socket: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.GameScreenController.socket",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "gamews"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:10
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:10
+    def at: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.at",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[play.api.mvc.PathBindable[String]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+
+}
