c0247930f98c075f9ef4c53ac06df9fec6508f52
Lin-Laura <3075229W@student.gla.ac.uk>
2026-02-23 21:52:50 +0000

    done #4，5

--- app/events/EndTurnClicked.java
+++ app/events/EndTurnClicked.java
@@ -1,26 +1,27 @@
 package events;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
 import akka.actor.ActorRef;
+import com.fasterxml.jackson.databind.JsonNode;
+import game.core.CoreGameState;
+import game.core.Owner;
 import structures.GameState;
 
-/**
- * Indicates that the user has clicked an object on the game canvas, in this case
- * the end-turn button.
- * 
- * { 
- *   messageType = “endTurnClicked”
- * }
- * 
- * @author Dr. Richard McCreadie
- *
- */
-public class EndTurnClicked implements EventProcessor{
+public class EndTurnClicked implements EventProcessor {
 
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		
-	}
 
+		CoreGameState core = gameState.core();
+
+		// (Story #5) mana drain: mana is cleared at the end of the current player's turn
+		Owner current = core.activePlayer();
+		core.getPlayer(current).setMana(0);
+
+		// Switch round
+		core.nextTurn();
+
+		// Story #4) mana gain: New turn starts mana = turnNumber + 1
+		int manaForThisTurn = core.turnNumber() + 1;
+		core.getPlayer(core.activePlayer()).setMana(manaForThisTurn);
+	}
 }
