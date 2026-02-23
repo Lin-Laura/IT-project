d877fb1ef15e0c74a2a450ddf69b49aac63e3288
FNT-FAN <3075456f@student.gla.ac.uk>
2026-02-23 06:22:43 +0000

    Update EndTurnClicked.java

--- app/events/EndTurnClicked.java
+++ app/events/EndTurnClicked.java
@@ -1,26 +1,69 @@
-package events;
+// package events;
+
+// import com.fasterxml.jackson.databind.JsonNode;
+
+// import akka.actor.ActorRef;
+// import structures.GameState;
+
+// /**
+//  * Indicates that the user has clicked an object on the game canvas, in this case
+//  * the end-turn button.
+//  * 
+//  * { 
+//  *   messageType = “endTurnClicked”
+//  * }
+//  * 
+//  * @author Dr. Richard McCreadie
+//  *
+//  */
+// public class EndTurnClicked implements EventProcessor{
+
+// 	@Override
+// 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
+		
+// 	}
+
+// }
+
+
+
+
+package events; 
 
 import com.fasterxml.jackson.databind.JsonNode;
-
 import akka.actor.ActorRef;
+import commands.BasicCommands;
+
 import structures.GameState;
+import structures.Player;
+import structures.Card;
+import structures.Deck;
+import structures.Hand;
 
-/**
- * Indicates that the user has clicked an object on the game canvas, in this case
- * the end-turn button.
- * 
- * { 
- *   messageType = “endTurnClicked”
- * }
- * 
- * @author Dr. Richard McCreadie
- *
- */
-public class EndTurnClicked implements EventProcessor{
+ 	
+public class EndTurnClicked implements EventProcessor {
 
-	@Override
-	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		
-	}
+    @Override
+    public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
+        if (gameState.isHumanTurn == false) {
+            return; 
+        }
 
+        Player humanPlayer = gameState.humanPlayer; 
+
+        Deck deck = humanPlayer.getDeck();
+        Hand hand = humanPlayer.getHand();
+        
+        if (deck.getCards().size() > 0) {
+            Card topCard = deck.getCards().remove(0); 
+            hand.getCards().add(topCard);             
+        }
+
+        humanPlayer.setMana(0);
+
+        gameState.isHumanTurn = false;
+        gameState.turnNumber = gameState.turnNumber + 1;
+
+        BasicCommands.addPlayer1Notification(out, "Opponent's Turn", 2);
+    }
 }
